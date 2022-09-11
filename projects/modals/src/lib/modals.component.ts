572c15db53a037f057695e878537ba0ca53a04f2
DmitryIgorevich <mr.bunakov1990@gmail.com>
2022-09-11 12:22:42 +0300

    first commit

---
+++ projects/modals/src/lib/modals.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-modals',
+  template: `
+    <p>
+      modals works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ModalsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
